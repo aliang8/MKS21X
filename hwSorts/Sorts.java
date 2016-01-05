879162c06dd84e097cf36c7ff82c14d5ef448870
Anthony Liang <aliang8@stuy.edu>
2016-01-05 18:53:34 -0500

    Added Bubble Sort

--- hwSorts/Sorts.java
+++ hwSorts/Sorts.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Sorts {
 
    public static void insertionSort(int[] data){
@@ -11,6 +13,7 @@ public class Sorts {
 		index-=1;
 	    }
 	    data[index]= value;
+	    printArray(data);
 	}
     }
 
@@ -29,10 +32,30 @@ public class Sorts {
 	    data[index] = data[start];
 	    data[start] = smallest;
 	    start++;
+	    printArray(data);
 	}
     }
 
+    public static void bubbleSort(int[]data) {
+	int counter = data.length;
+	int temp;
+	while (counter != 0) {
+	    for (int i = 1; i < counter; i++) {
+		if (data[i-1] > data[i]) {
+		    temp = data[i-1];
+		    data[i-1] = data[i];
+		    data[i] = temp;
+		}
+	    }
+	    counter--; 
+	    printArray(data);
+	}
+    }
+
+
     public static void  printArray(int[] data) {
+	System.out.println(Arrays.toString(data));	
+	/*	
 	String out = "[";
 	for (int i = 0; i < data.length; i++) {
 	    if (i == data.length-1) {
@@ -43,21 +66,30 @@ public class Sorts {
 	}
 	out += "]";
 	System.out.println(out);
+	*/
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[]args) {
 	int[] test1 = {10,8,5,6,7,12};
+	System.out.println("Insertion Sort Test");
 	insertionSort(test1);
 	printArray(test1);
 	
 	int[] test2 = {1,24,5,654,52};
+	System.out.println("Selection Sort Test");	
 	selectionSort(test2);
 	printArray(test2); 
 
 	int[] test3 = {12};
+	System.out.println("Selection Sort Test 2");
 	selectionSort(test3);
 	printArray(test3);
+
+	int[] test4 = {1,2,9,5,0,3};
+	System.out.println("Bubble Sort Test");
+	bubbleSort(test4);
+	printArray(test4);
     }
     
 }
